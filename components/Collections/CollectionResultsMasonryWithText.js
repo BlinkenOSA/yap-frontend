07c7fba78ca09a4c83585acdeea7b66af8c3deb3
JoshBone <jozsef.bone@gmail.com>
2024-06-21 10:07:42 +0200

    Rollback

--- components/Collections/CollectionResultsMasonryWithText.js
+++ components/Collections/CollectionResultsMasonryWithText.js
@@ -72,7 +72,11 @@ const CollectionResultsMasonryWithText = ({data, isMobile=false}) => {
         >
           <Row>
             <Col xs={isMobile ? 24 : 12} style={isMobile ? {height: '200px'} : undefined}>
-              <img alt="thumbnail" src={selectedRecord['thumbnail'] ? selectedRecord['thumbnail'] : '/images/recordEmpty.svg'} />
+              <Image
+                layout={'fill'}
+                objectFit={'contain'}
+                src={selectedRecord['thumbnail'] ? selectedRecord['thumbnail'] : '/images/recordEmpty.svg'}
+              />
             </Col>
             <Col xs={isMobile ? 24 : 12}>
               <div className={isMobile ? style.PopupContentWrapMobile : style.PopupContentWrap} >
--- components/Search/ResultPage/ResultPageList.js
+++ components/Search/ResultPage/ResultPageList.js
@@ -75,7 +75,11 @@ const ResultPageList = ({data, displayOnMapID, onClickDisplayOnMap, urlParams}) 
     } else {
       return (
         <a href={`/record/${d.id}`} target={'_blank'}>
-          <img alt="thumbnail" src='/images/recordEmpty.png'/>
+          <Image
+            layout={'fill'}
+            objectFit={'cover'}
+            objectPosition={'top right'}
+            src='/images/recordEmpty.png'/>
         </a>
       )
     }
